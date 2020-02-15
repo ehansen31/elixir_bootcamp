09f6b0284b35c1e3debafffd4920fcc669ff9cff
erik <ehansen1231@gmail.com>
2020-02-14 21:44:50 -0500

    git ignore and cards

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,24 +1,18 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    "hi there!"
-    # :world
-  end
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values=["Ace", "Two", "Three", "Four", "Five"]
+    suits=[]
   end
 
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+
+  
 end
