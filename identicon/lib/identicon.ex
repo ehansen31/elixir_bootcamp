999b5c1fa743d09f11dbbe6bb985317ae33594f5
Erik Hansen <e.hansen31@live.com>
2020-02-16 07:20:53 -0500

    color generation complete

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,10 +2,20 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list()
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
