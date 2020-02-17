e59302605b7816f11c1cc6aef28b7f0e4392b174
erik <ehansen1231@gmail.com>
2020-02-16 21:32:30 -0500

    shelf

--- discuss/config/test.exs
+++ discuss/config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "user",
+  password: "pass",
   database: "discuss_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
