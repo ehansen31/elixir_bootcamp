f6829432df89dbcafe07b1a5c3b933e53a2bafbb
Erik Hansen <e.hansen31@live.com>
2020-02-20 22:19:53 -0500

    start on comments build out

---
+++ discuss/web/channels/comments_channel.ex
@@ -0,0 +1,3 @@
+defmodule Discuss.CommentsChannel do
+  use Discuss.Web, :channel
+end
--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -11,6 +11,11 @@ defmodule Discuss.TopicController do
     render(conn, "index.html", topics: topics)
   end
 
+  def show(conn, %{"id" => topic_id} = params) do
+    topic = Repo.get!(Topic, topic_id)
+    render(conn, "show.html", topic: topic)
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
