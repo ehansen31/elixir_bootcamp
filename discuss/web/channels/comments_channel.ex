25f04bd2c364accb8b54a30359d8f420011055ba
Erik Hansen <e.hansen31@live.com>
2020-02-20 23:16:51 -0500

    save button and web socket

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,3 +1,11 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
+
+  def join(name, _params, socket) do
+    {:ok, %{hey: "there"}, socket}
+  end
+
+  def handle_in(name, message, socket) do
+    {:reply, :ok, socket}
+  end
 end
